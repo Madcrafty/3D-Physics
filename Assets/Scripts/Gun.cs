58cbc1f79297bb29ee91c8182a1f0e5fefa1072b
madcrafty <madcrafty@icloud.com>
2021-02-26 17:01:40 +1100

    added shotty boi

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gun : MonoBehaviour
+{
+    public float shotCost = 5.0f;
+    public float effectTime = 0.1f;
+    public float maxCapacity = 100;
+    public float rechargeRate = 1;
+
+    private float capacity;
+    private float elapsedLaserTime = 0;
+    private Vector3 targetPos;
+    private Vector3 originPoint;
+    private Collider barrel;
+    private LineRenderer laser;
+    private Slider chargeMeter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        capacity = maxCapacity;
+        chargeMeter = transform.GetChild(3).GetChild(0).GetComponent<Slider>();
+        laser = GetComponent<LineRenderer>();
+        barrel = transform.GetChild(2).GetComponent<Collider>();
+        originPoint = barrel.bounds.center;
+        originPoint.y -= barrel.bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        originPoint = barrel.bounds.center;
+        originPoint.y -= barrel.bounds.extents.y;
+        laser.SetPosition(0, originPoint);
+        if (capacity < maxCapacity)
+        {
+            capacity += rechargeRate;
+        }
+        if (capacity > maxCapacity)
+        {
+            capacity = maxCapacity;
+        }
+        chargeMeter.value = capacity / maxCapacity;
+        if (Input.GetMouseButtonDown(0) && capacity >= shotCost)
+        {
+            Shoot();
+            capacity -= shotCost;
+        }
+        if (laser.enabled)
+        {
+            elapsedLaserTime += Time.deltaTime;
+            if (elapsedLaserTime >= effectTime)
+            {
+                laser.enabled = false;
+            }
+        }
+        else if (elapsedLaserTime > 0)
+        {
+            elapsedLaserTime = 0;
+        }
+    }
+    void Shoot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        Physics.Raycast(ray, out hitInfo, 500);
+        if (hitInfo.point == null)
+        {
+            targetPos = Camera.main.transform.forward * 500;
+        }
+        else
+        {
+            targetPos = hitInfo.point;
+        }
+        laser.SetPosition(1, targetPos);
+        laser.enabled = true;
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float jumpForce = 10.0f;
+    public float cyoteTime = 0.0f;
+    public float lookSpeed = 2.0f;
+
+    private Rigidbody rb;
+    private Collider col;
+    private Camera cam;
+    private Gun gun;
+    private float lookXLimit = 90.0f;
+    
+    private bool grounded;
+    private float elapsedCyoteTime = 0;
+    private Ray ray;
+
+    private float rotationX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        cam = transform.GetChild(0).GetComponent<Camera>();
+        gun = transform.GetChild(0).GetChild(0).GetComponent<Gun>();
+        Cursor.lockState = CursorLockMode.Locked;
+        ray = new Ray(col.bounds.center, Vector3.down);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        cam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+
+        rb.AddForce(transform.forward * Input.GetAxis("Vertical") * speed, ForceMode.Impulse);
+        rb.AddForce(transform.right * Input.GetAxis("Horizontal") * speed, ForceMode.Impulse);
+
+        if (grounded && Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * speed, ForceMode.Impulse);
+        }
+    }
+    private void FixedUpdate()
+    {
+        CheckGroundedState();
+    }
+    void CheckGroundedState()
+    {
+        RaycastHit hit;
+        ray.origin = col.bounds.center;
+        if (col.GetType() == typeof(CapsuleCollider) || col.GetType() == typeof(SphereCollider))
+        {
+            Physics.SphereCast(ray, col.bounds.extents.x, out hit, col.bounds.extents.y + 0.1f);
+        }
+        else
+        {
+            Physics.Raycast(ray, out hit, col.bounds.extents.y + 0.1f);
+        }
+        
+        if (hit.transform == null)
+        {
+            elapsedCyoteTime += Time.fixedDeltaTime;
+            if (elapsedCyoteTime >= cyoteTime)
+            {
+                grounded = false;
+            }
+        }
+        else if (!grounded || elapsedCyoteTime > 0)
+        {
+            grounded = true;
+            elapsedCyoteTime = 0;
+        }
+
+    }
+}
